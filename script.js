21a75405f7f5c3760ea529d2cec7116f292b0d41
maadurgastudio <shubham229patel@gmail.com>
2026-08-06 11:31:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,37 @@ let name = document.querySelector("input[type='text']").value;
 let mobile = document.querySelector("input[type='number']").value;
 let date = document.querySelector("input[type='date']").value;
 
-alert(
-"Booking Received\n\n" +
-"Name: " + name +
-"\nMobile: " + mobile +
-"\nDate: " + date +
-"\n\nMD Studio Booking"
-);
+let selects = document.querySelectorAll("select");
+
+let eventType = selects[0].value;
+let packageName = selects[1].value;
+
+
+let prices = {
+"4K Videography":10000,
+"DSLR Photography":8000,
+"Drone Shoot":8000,
+"4K + DSLR":16999,
+"4K + Drone":16999,
+"DSLR + Drone":14999,
+"4K + DSLR + Drone":24999
+};
+
+
+let price = prices[packageName] || 0;
+
+
+let bookingId = "MD" + Math.floor(Math.random()*100000);
+
+
+localStorage.setItem("bookingId",bookingId);
+localStorage.setItem("name",name);
+localStorage.setItem("mobile",mobile);
+localStorage.setItem("date",date);
+localStorage.setItem("package",packageName);
+localStorage.setItem("price",price);
+
+
+window.location.href="receipt.html";
 
 });
